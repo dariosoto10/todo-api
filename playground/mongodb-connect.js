c1e00fb4815cd1e0f7d71c3855edb52976893d3c
Dario Alejandro <sotodario3@gmail.com>
2017-10-22 16:22:37 -0300

    Add POST /todos route and refactor mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -3,7 +3,7 @@ const { MongoClient, ObjectID } = require('mongodb')
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if(err) {
-        return console.logs(`Unable to connect to MongoDB server`)
+        return console.log(`Unable to connect to MongoDB server`)
     } else {
         console.log(`Connected to MongoDB server`)
 
